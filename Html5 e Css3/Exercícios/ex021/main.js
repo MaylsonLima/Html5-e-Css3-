b08430a5282abc4fc643ae928a9f986cb20475c3
Html5 e Css3 <m.limacodes@gmail.com>
2025-09-16 20:53:55 -0300

    1.0

---
+++ Html5 e Css3/Exercícios/ex021/main.js
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snorlax informations</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Manufacturing+Consent&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <style>
+    
+    .manufacturing-consent-regular {
+  font-family: "rubik", system-ui;
+  font-weight: 400;
+  font-style: normal;
+}
+
+        body {
+            background-image: linear-gradient(#5E94A7,#EFE4D7);
+            background-attachment: fixed;
+            font-family: rubik;
+        }
+        .branca {
+            background-color: white;
+            padding: 20px 20px 20px 20px;
+            border-radius: 20px;
+            box-shadow: 3px 3px 4px #B8AFA5;
+            margin: 30px ;
+        }
+        img {
+            width:300px;
+            height:300px;
+            outline-width: 5px;
+        }
+        #imagem {
+            display: flex;
+            justify-content: center;
+        }
+        h1 {
+            color: #FF984F;
+            text-align: center;
+            
+        }
+        h2 {
+            color:#FF4F5F;
+        }
+        h3 {
+            color:#AB323D;
+        }
+        div {
+            display: flex;
+            justify-content: center;
+            text-align: center;
+        }
+    </style>
+        
+    </style>
+</head>
+<body>
+    <main class="branca">
+        <div>
+        <h1>Snorlax O Pokémon do Tipo Normal</h1>
+        </div>
+        
+        <div>
+            <h2>descrição do Pokémon</h2>
+        </div>
+        
+        <div>
+            <p>snorlax,o Pokémon do tipo normal,a medida que seu corpo arredondado aumenta se torna cada vez mais perigoso,esse Pokémon pode comer de tudo sem passar mau ou ter indigestão</p>
+        </div>
+        
+        <div id="imagem">
+            <img src="snorlax-original-475x475.png" alt="snorlax o sinistro" />
+        </div>
+        </main>
+        
+        <main class="branca">
+            
+            <h1>Informações Adicionais </h1>
+        
+        <div>
+            <h2>status base</h2>
+            <ul>
+                <li>HP 160</li>
+                <li>ATK 110</li>
+                <li>DEF 65</li>
+                <li>ATK.ES 65</li>
+                <li>DEF.ES 110</li>
+                <li>VEL 30</li>
+            </ul>
+        </div>
+        
+        <div>
+            <h2>Onde conseguir snorlax?</h2>
+        </div>
+        
+        <div>
+            <p>a varias formas e varias maneiras de conseguir um snorlax, já que são varios jogos de Pokémon e muitas rotas,mas eu consigo citar algumas aqui</p>
+        </div>
+        
+        <div>
+            <h3>Rotas em Kanto</h3>
+        </div>
+        <div>
+            <p>é possível conseguir um snorlax usando a pokeflauta,nas rotas 12 e 16</p>
+        </div>
+        
+        <div>
+            <h3>Munchilex em Sinnoh</h3>
+        </div>
+        <div>
+            <p>Você pode pegar um Munchilex e evoluílo pra snorlax em sinnoh,basta pegar o item Sthaler Mel e após bater numa árvore,a chance é baixa,mas existe</p>
+        </div>
+        </main>
+</body>
+</html>
+  
